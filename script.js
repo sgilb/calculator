e57c0e8d61ef7ff9af8adf2afd829f852fa50ee2
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-30 16:12:34 +0000

    Add operate function to call operators by symbol

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let firstNumber, operator, secondNumber;
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +14,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(first, operator, second) {
+  switch(operator) {
+    case "+":
+      add(first, second);
+      break;
+    case "-":
+      subtract(first, second);
+      break;
+    case "*":
+      multiply(first, second);
+      break;
+    case "/":
+      divide(first, second);
+      break;   
+    default:
+      return "Operator not recognised"
+  }
 }
