912118929b05d3ef4d6f186696f56ba49576bf6b
sgilb <32495429+sgilb@users.noreply.github.com>
2024-01-01 18:09:18 +0000

    Add logic to display and clear number input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let firstNumber, operator, secondNumber;
+const buttons = document.querySelectorAll(".action");
 
 function add(a, b) {
   return a + b;
@@ -17,7 +18,7 @@ function divide(a, b) {
 }
 
 function operate(first, operator, second) {
-  switch(operator) {
+  switch (operator) {
     case "+":
       return add(first, second);
     case "-":
@@ -27,6 +28,28 @@ function operate(first, operator, second) {
     case "/":
       return divide(first, second);
     default:
-      return "Operator not recognised"
+      return "Operator not recognised";
   }
 }
+
+function writeNumber(input, canvas) {
+  if (
+    Number.isInteger(Number(input)) ||
+    (input === "." && !canvas.innerText.includes("."))
+  ) {
+    canvas.innerText += input;
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const screen = document.getElementById("screen");
+    const element = e.target;
+
+    if (element.classList.contains("clear")) {
+      screen.innerText = "";
+    } else if (element.classList.contains("input")) {
+      writeNumber(element.innerText, screen);
+    }
+  });
+});
