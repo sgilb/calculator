675fad17918ece2176af43d6f3c43eff9ed1ec03
sgilb <32495429+sgilb@users.noreply.github.com>
2024-01-02 23:50:31 +0000

    Allow multi-operator input and refactor

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let displayValue, firstNumber, secondNumber, operator;
 const buttons = document.querySelectorAll(".action");
+const screen = document.getElementById("screen");
 
 function add(a, b) {
   return a + b;
@@ -18,12 +19,15 @@ function divide(a, b) {
 }
 
 function operate(first, operator, second) {
+  first = Number(first);
+  second = Number(second);
+  
   switch (operator) {
     case "+":
       return add(first, second);
     case "-":
       return subtract(first, second);
-    case "*":
+    case "x":
       return multiply(first, second);
     case "/":
       return divide(first, second);
@@ -32,40 +36,61 @@ function operate(first, operator, second) {
   }
 }
 
-function writeNumber(input, canvas) {
-  let content = canvas.innerText;
-  if (
-    Number.isInteger(Number(input)) ||
-    (input === "." && !content.includes("."))
-  ) {
+function write(input) {
+  let content = screen.innerText;
+  const lastCharIsNumber = Number.isInteger(
+    Number(removeWhitespace(content).slice(-1))
+  );
+  const inputIsNumber = Number.isInteger(Number(input));
+
+  if (input.match(/[-+/x]/) && lastCharIsNumber) {
+    content += ` ${input} `;
+  } else if (inputIsNumber || (input === "." && !content.includes("."))) {
     content += input;
   }
-  canvas.innerText = content;
+  refreshScreen(content);
 }
 
-function writeOperator(input, canvas) {
-  let content = canvas.innerText;
+// TODO
+// function solve(equation) {
+//   const elements = equation.split(" ");
+//   let total = 0;
 
-  if (
-    !content.match("[-+/x]") &&
-    Number.isInteger(Number(content.slice(-1)))
-  ) {
-    content += input;
-  }
-  canvas.innerText = content;
+//   for (let i = 0; i < elements.length; i += 3) {
+//     let first = elements[i];
+//     let operator = elements[i+1];
+//     let second = elements[i+2];
+
+//     total += operate(first, operator, second);
+//     console.log(total);
+//   }
+//   refreshScreen(total);
+// }
+
+function removeWhitespace(string) {
+  return string.replace(/\s/g, "");
+}
+
+function refreshScreen(content) {
+  displayValue = content;
+  screen.innerText = displayValue;
 }
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const screen = document.getElementById("screen");
-    const element = e.target;
+    const classes = e.target.classList;
 
-    if (element.classList.contains("clear")) {
-      screen.innerText = ""; // Clear display
-    } else if (element.classList.contains("input")) {
-      writeNumber(element.innerText, screen);
-    } else if (element.classList.contains("operator")) {
-      writeOperator(element.innerText, screen);
+    switch (true) {
+      case classes.contains("clear"):
+        screen.innerText = ""; // Clear display
+        break;
+      case classes.contains("input") || classes.contains("operator"):
+        write(e.target.innerText);
+        break;
+      case classes.contains("equals"):
+        solve(displayValue);
+        break;
     }
   });
 });
